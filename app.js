797fbdaa0ea3b9c9b9ad124e691ca459600052c3
Vamshi925 <kvamshireddy5@gmail.com>
2026-06-09 14:38:00 +0000

    initial commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Hello from AWS DevOps Project Phase 1");
+});
+
+app.get("/health", (req, res) => {
+    res.status(200).send("Healthy");
+});
+
+app.listen(3000, () => {
+    console.log("Server running on port 3000");
+});
